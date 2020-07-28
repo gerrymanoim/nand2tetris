7be4b7eeb99a608861f7379e360608c85970a3f9
gerrymanoim <gerrymanoim@gmail.com>
2020-07-27 23:08:02 -0400

    Chapter 4 done

--- software/projects/04/fill/Fill.asm
+++ software/projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(CHECKKEYPRESS)
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    @color
+    M=-1
+    @DRAW
+    0;JMP
+
+(WHITE)
+    @color
+    M=0
+    @DRAW
+    0;JMP
+
+(DRAW)
+    @SCREEN
+    D=A
+    @addr
+    M=D //sets addr to screen value
+
+    @8192
+    D=A
+    @n  
+    M=D //sets n to the max screen pixel
+    
+    @i
+    M=0
+
+    @DRAWLOOP
+    0;JMP
+
+(DRAWLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @CHECKKEYPRESS
+    D;JGE
+
+    @color
+    D=M
+    @addr
+    A=M
+    M=D
+
+    @i
+    M=M+1
+    @addr
+    M=M+1
+
+    @DRAWLOOP
+    0;JMP
